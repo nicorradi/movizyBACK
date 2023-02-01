14f3fdfd6d5a3b1b3870fefd27909dbef43fbc81
nicorradi <nicorradi@hotmail.com>
2023-02-01 16:30:08 +0100

    try fix

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 require('dotenv').config();
 const fetch = require('node-fetch');
 
-const API_KEY = process.env.TMDB_API_KEY
+const TMDB_API_KEY = process.env.TMDB_API_KEY
 
 router.get('/', (req, res) => {
     res.render('index.html')
@@ -11,7 +11,7 @@ router.get('/', (req, res) => {
 
 router.get("/movies", (req, res) => {
     console.log('route');
-    fetch(`https://api.themoviedb.org/3/discover/movie?api_key=${API_KEY}`)
+    fetch(`https://api.themoviedb.org/3/discover/movie?api_key=${TMDB_API_KEY}`)
     .then(response => response.json())
     .then(data => { 
         console.log(data);
